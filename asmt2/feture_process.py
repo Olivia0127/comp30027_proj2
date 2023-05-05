4d6ae5b8d13fd69c1bf10a68e304307887a15201
Alserial <95892981+Alserial@users.noreply.github.com>
2023-05-05 16:40:19 +1000

    一坨大便

--- asmt2/feture_process.py
+++ asmt2/feture_process.py
@@ -11,17 +11,20 @@ import scipy
 from scipy.sparse import csr_matrix
 from sklearn.feature_selection import SelectKBest, mutual_info_classif
 from sklearn.preprocessing import OneHotEncoder
-def process_OneHotEncoder(train_df,test_df,column_name:str):
+
+
+
+def process_OneHotEncoder(df,column_name:str):
     encoder = OneHotEncoder(sparse=False)
-    one_hot_train = encoder.fit_transform(train_df[[column_name]])
-    one_hot_train_df = pd.DataFrame(one_hot_train, columns=encoder.get_feature_names_out([column_name]))
-    one_hot_test = encoder.fit_transform(test_df[[column_name]])
-    one_hot_test_df = pd.DataFrame(one_hot_test, columns=encoder.get_feature_names_out([column_name]))
-    return one_hot_train_df, one_hot_test_df
+    one_hot = encoder.fit_transform(df[[column_name]])
+    one_hot_df = pd.DataFrame(one_hot, columns=encoder.get_feature_names_out([column_name]))
+    return one_hot_df
+    
 def process_OneHotEncoder_pd(train_df,test_df,column_name:str):
     one_hot_encoded_train_df = pd.get_dummies(train_df, columns=[column_name])
     one_hot_encoded_test_df = pd.get_dummies(test_df, columns=[column_name])
     return one_hot_encoded_train_df,one_hot_encoded_test_df
+
 def docclass_preprocess(train, test, threshold):
     #change some type of class into other to decrease the dimension of matrix
     data = train.value_counts()
